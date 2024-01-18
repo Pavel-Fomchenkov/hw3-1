ced27bcc6ea72c6138c12a85a2970c41f56bbe20
Pavel-Fomchenkov <donotwritemail@gmail.com>
2024-01-18 23:27:49 +0300

    Initial commit

---
+++ src/test/java/pro/sky/javacoursepart3/hw31/Hw31ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.javacoursepart3.hw31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
